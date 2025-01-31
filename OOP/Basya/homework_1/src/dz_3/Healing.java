4132e327af2a1797a96fd25136bfaac506e1c031
Amanbaev Bayastan <maintrot0525@gmail.com>
2025-01-31 19:57:28 +0600

    сделал заранее 3 задание

---
+++ OOP/Basya/homework_1/src/dz_3/Healing.java
@@ -0,0 +1,8 @@
+package dz_3;
+
+public class Healing implements MagicSpell{
+    @Override
+    public void cast() {
+        System.out.println("Вы направляете на себя заклинание исцеления. Вы чувствуете как прежние раны больше ноют");
+    }
+}
---
+++ OOP/Basya/homework_1/src/dz_3/Shield.java
@@ -0,0 +1,8 @@
+package dz_3;
+
+public class Shield implements MagicSpell{
+    @Override
+    public void cast() {
+        System.out.println("Вы создаете барьерный щит вокруг себя. Никто ныне вас ранить не в силе");
+    }
+}
