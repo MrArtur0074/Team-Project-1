c6239124ab1bcd14127012a06b7c37ff8a260d36
Amanbaev Bayastan <maintrot0525@gmail.com>
2025-03-13 10:49:24 +0600

    add opportunity to registrate and login

--- PLL_projects/E-commerce_website/front-end/pll_frontend/src/components/SignIn.jsx
+++ PLL_projects/E-commerce_website/front-end/pll_frontend/src/components/SignIn.jsx
@@ -1,15 +1,67 @@
 import React from 'react';
 import './SignIn.css';
+import { useState, useContext} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../App';
+
 
 const SignIn = () => {
+  const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const navigate = useNavigate()
+	const [user, setUser] = useContext(UserContext)
+
+	function setInputUsername(event) {
+		setUsername(event.target.value)
+	}
+
+	function setInputPassword(event) {
+		setPassword(event.target.value)
+	}
+
+	function backFromPage() {
+		navigate('/')
+	}
+
+	async function sendData(event) {
+		event.preventDefault()
+
+		const responce = axios.post('', {
+			username: username,
+			password: password,
+		})
+		.then((res) => {
+			localStorage.setItem('access_token', res.data.access)
+			localStorage.setItem('refresh_token', res.data.refresh)
+			localStorage.setItem('user_id', res.data.user_id)
+
+			setUser(true)
+
+			navigate('/')
+		})
+		.catch((error) => {
+			if (error.response) {
+				console.error('Authentication failed. Server responded with:', error.response.data)
+			} else if (error.request) {
+				console.error('No response received from the server. Is the server running?')
+			} else {
+				console.error('Error setting up the request:', error.message)
+			}
+		})
+	}
+
   return (
     <div className="signin">
       <h2>Вход</h2>
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Пароль" />
-        <button type="submit">Войти</button>
+      <form onSubmit={sendData} method='post'>
+			  <label htmlFor='username'>Username: </label>
+        <input onChange={setInputUsername} name='username' onChange={setInputUsername} type="username" placeholder="enter your username" />
+				<label htmlFor='password'>Password: </label>
+        <input onChange={setInputPassword} name='password' onChange={setInputPassword} type="password" placeholder="enter your password" />
+        <input type="submit" value="submit"/>
       </form>
+			<button onClick={backFromPage}>Back</button>
     </div>
   );
 };
--- PLL_projects/E-commerce_website/front-end/pll_frontend/src/components/SignUp.jsx
+++ PLL_projects/E-commerce_website/front-end/pll_frontend/src/components/SignUp.jsx
@@ -1,16 +1,70 @@
 import React from 'react';
 import './SignUp.css';
+import { useNavigate, UseNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 
 const SignUp = () => {
+
+  const [username, setUsername] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const navigate = useNavigate()
+
+	function setInputUsername(event) {
+		setUsername(event.target.value)
+	}
+
+	function setInputEmail(event) {
+		setEmail(event.target.value)
+	}
+
+	function setInputPassword(event) {
+		setPassword(event.target.value)
+	}
+
+	function backFromPage() {
+		navigate('/')
+	}
+
+	async function sendData(event) {
+		event.preventDefault()
+
+		const responce = axios.post('', {
+			username: username,
+			email: email,
+			password: password,
+		})
+		.then((res) => {
+			localStorage.setItem('user_id', res.data.id)
+
+			navigate('/')
+		})
+		.catch((error) => {
+			if (error.response) {
+				console.error('Authentication failed. Server responded with:', error.response.data)
+			} else if (error.request) {
+				console.error('No response received from the server. Is the server running?')
+			} else {
+				console.error('Error setting up the request:', error.message)
+			}
+		})
+	}
+
   return (
     <div className="signup">
       <h2>Регистрация</h2>
-      <form>
-        <input type="text" placeholder="Имя" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Пароль" />
-        <button type="submit">Зарегистрироваться</button>
+      <form onSubmit={sendData} method='post'>
+				<label htmlFor='username'>Username: </label>
+        <input onChange={setInputUsername} type="text" name='username' placeholder="Enter your name" />
+				<label htmlFor='email'>Email: </label>
+        <input onChange={setInputEmail} type="email" name='email' placeholder="Enter your email" />
+				<label htmlFor='password'>Password: </label>
+        <input onChange={setInputPassword} type="password" name='password' placeholder="Enter your password" />
+        <input type="submit" value="submit"/>
       </form>
+			<button onClick={backFromPage}>Back</button>
     </div>
   );
 };
